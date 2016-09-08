b940ebdc784ebb3fd295e7e2d1313a742d93c4ee
Rocco Bowling <rocco@chimerasw.com>
2016-09-08 11:47:01 -0400

    WIP: initial version with particles working in Unity UI

---
+++ Assets/Code/FPSDisplay.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class FPSDisplay : MonoBehaviour
+{
+	private ParticleSystem testParticleSystem = null;
+	float deltaTime = 0.0f;
+
+	void Start() {
+		// find the active partcile system
+		foreach (GameObject obj in SceneManager.GetActiveScene().GetRootGameObjects()) {
+			if (obj.activeSelf == true) {
+				ParticleSystem[] systems = obj.GetComponentsInChildren<ParticleSystem> ();
+				if (systems.Length > 0) {
+					testParticleSystem = systems [0];
+				}
+			}
+		}
+	}
+ 
+	void Update()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+	}
+ 
+	void OnGUI()
+	{
+		int w = Screen.width, h = Screen.height;
+ 
+		GUIStyle style = new GUIStyle();
+ 
+		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 2 / 100;
+		style.normal.textColor = Color.red;
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		string text = string.Format("{0:0.0} ms ({1:0.} fps) ({2} particles)", msec, fps, testParticleSystem.maxParticles);
+		GUI.Label(rect, text, style);
+	}
+}
